ac30d4f4aa460d0bf62493c6b87cabb3e5a6b30a
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-02-10 17:53:36 +0000

    have a running grunt build!

--- fairwaytech_tutorial/gruntfile.js
+++ fairwaytech_tutorial/gruntfile.js
@@ -2,18 +2,44 @@
  * New node file
  */
 module.exports = function(grunt) {
-	var banner = '/*\n<%= pkg.name %> <%= pkg.version %> - <%= pkg.description %>\n<%= pkg.repository.url %>\nBuilt on <%= grunt.template.today("yyyy-mm-dd") %>\n*/\n';
-		grunt.initConfig({
-			pkg: grunt.file.readJSON('package.json'),
-	        jshint: {
-	            files: ['gruntfile.js', 'src/*.js'],
-	            options: {
-	                maxlen: 80,
-	                quotmark: 'single'
-	            }
-	        }
-		});
-		grunt.loadNpmTasks('grunt-contrib-jshint');
-		grunt.registerTask('default', ['jshint']);
+	var banner = '/*\n<%= pkg.name %> <%= pkg.version %> - ';
+	banner += '<%= pkg.description %>\n<%= pkg.repository.url %>\n';
+	banner += 'Built on <%= grunt.template.today("yyyy-mm-dd") %>\n*/\n';
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
+		jshint : {
+			files : [ 'gruntfile.js', 'src/*.js' ],
+			options : {
+				maxlen : 80,
+				quotmark : 'single'
+			}
+		},
+		concat: {
+			options: {
+				separator: ';\n',
+				banner: banner
+			},
+			build: {
+				files: [{
+					src: ['src/*.js'], 
+					dest: 'build/<%= pkg.name %>.js'
+				}]
+			}
+		},
+        uglify: {
+            options: {
+                banner: banner,
+            },
+            build: {
+                files: { 
+                    'build/<%= pkg.name %>.min.js': 
+                        ['build/<%= pkg.name %>.js'],
+                }
+            }
+        },
+	});
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', [ 'jshint','concat','uglify' ]);
 };
-
