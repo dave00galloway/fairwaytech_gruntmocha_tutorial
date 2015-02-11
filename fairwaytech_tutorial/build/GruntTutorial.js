a50040daa75d9786da1f7a99047b6b9e9ccfbdbb
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-02-11 13:58:18 +0000

    grunt running ok - maybe some ignores todo!

---
+++ fairwaytech_tutorial/build/GruntTutorial.js
@@ -0,0 +1,28 @@
+/*
+GruntTutorial 0.1.0 - The demo project that accompanies my 'Understanding Grunt' tutorial
+https://github.com/craigmaslowski/Grunt-Tutorial.git
+Built on 2015-02-10
+*/
+/**
+ * http://fairwaytech.com/2014/01/understanding-grunt-part-1-installation-linting-concatenation-and-minification/
+ */
+module.exports = {
+	add : function(a, b) {
+		return a + b;
+	},
+	subtract : function(a, b) {
+		return a - b;
+	}
+
+};;
+/**
+ * New node file
+ */
+module.exports = {
+	add : function(a, b) {
+		return a + b;
+	},
+	subtract : function(a, b) {
+		return a - b;
+	}
+};
