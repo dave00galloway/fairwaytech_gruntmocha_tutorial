a3bbb5655b79ef9242572834335e985d9b70062f
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-02-11 14:40:25 +0000

    no changes to test, but did add task to split linting task

--- fairwaytech_tutorial/gruntfile.js
+++ fairwaytech_tutorial/gruntfile.js
@@ -5,52 +5,67 @@ module.exports = function(grunt) {
 	var banner = '/*\n<%= pkg.name %> <%= pkg.version %> - ';
 	banner += '<%= pkg.description %>\n<%= pkg.repository.url %>\n';
 	banner += 'Built on <%= grunt.template.today("yyyy-mm-dd") %>\n*/\n';
-	grunt.initConfig({
-		pkg : grunt.file.readJSON('package.json'),
-		jshint : {
-			files : [ 'gruntfile.js', 'src/*.js' ],
-			options : {
-				maxlen : 80,
-				quotmark : 'single'
-			}
-		},
-		concat: {
-			options: {
-				separator: ';\n',
-				banner: banner
-			},
-			build: {
-				files: [{
-					src: ['src/*.js'], 
-					dest: 'build/<%= pkg.name %>.js'
-				}]
-			}
-		},
-        uglify: {
-            options: {
-                banner: banner,
-            },
-            build: {
-                files: { 
-                    'build/<%= pkg.name %>.min.js': 
-                        ['build/<%= pkg.name %>.js'],
-                }
-            }
-        },
-        simplemocha: {
-            options: {
-                globals: ['expect'],
-                timeout: 3000,
-                ignoreLeaks: false,
-                ui: 'bdd',
-                reporter: 'tap'
-            },
-            all: { src: ['tests/**/*.js'] }
-        }
-	});
+	grunt
+			.initConfig({
+				pkg : grunt.file.readJSON('package.json'),
+				jshint : {
+					files : [ 'gruntfile.js', 'src/*.js' ],
+					options : {
+						maxlen : 80,
+						quotmark : 'single'
+					},
+				    dev: ['gruntfile.js', 'tests/*.js'],
+				    app:  ['src/**/*.js']
+				},
+				concat : {
+					options : {
+						separator : ';\n',
+						banner : banner
+					},
+					build : {
+						files : [ {
+							src : [ 'src/*.js' ],
+							dest : 'build/<%= pkg.name %>.js'
+						} ]
+					}
+				},
+				uglify : {
+					options : {
+						banner : banner,
+					},
+					build : {
+						files : {
+							'build/<%= pkg.name %>.min.js' :
+								[ 'build/<%= pkg.name %>.js' ],
+						}
+					}
+				},
+				simplemocha : {
+					options : {
+						globals : [ 'expect' ],
+						timeout : 3000,
+						ignoreLeaks : false,
+						ui : 'bdd',
+						reporter : 'tap'
+					},
+					all : {
+						src : [ 'tests/**/*.js' ]
+					}
+				},
+		        watch: {
+		            scripts: {
+		                files: ['gruntfile.js', 'src/*.js', 'tests/**/*.js'],
+		                tasks: ['development']
+		            }
+		        }
+			});
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-simple-mocha');
-	grunt.registerTask('default', [ 'jshint','simplemocha','concat','uglify' ]);
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
+	grunt.registerTask('default',
+			[ 'jshint', 'simplemocha', 'concat', 'uglify' ]);
+	grunt.registerTask('development', [ 'jshint', 'simplemocha' ]);
 };
--- fairwaytech_tutorial/src/string.js
+++ fairwaytech_tutorial/src/string.js
@@ -9,3 +9,4 @@ module.exports = {
 		return a.split('').reverse().join('');
 	}
 };
+//comment
